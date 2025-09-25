67b9666f3bd0a52faceec789616c31fccffb8afe
maxim-ld <ayinde779@gmail.com>
2025-09-25 17:40:47 +0100

    feat: add error handler and rate limit middlewares

---
+++ src/middlewares/error_handler.ts
@@ -0,0 +1,67 @@
+import { NextFunction, Request, Response } from "express"
+import { IApiResponse } from "../types/api_response"
+import { AppError } from "../utils/errors"
+import { config } from "../config"
+import { logger } from "../utils/logger"
+
+export const notFoundHandler = (req: Request, res: Response): void => {
+    const response: IApiResponse = {
+        success: false,
+        message: `Route ${req.originalUrl} not found!`,
+        error: {
+            code: 'NOT_FOUND'
+        }
+    }
+    res.status(404).json(response)
+}
+
+// Async wrapper
+type ExpressHandler = (req: Request, res: Response, next: NextFunction) => Promise<any> | void
+    
+export const asyncHandler = (fn: ExpressHandler) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(fn(req, res, next)).catch(next)
+    }
+}
+
+export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction): void => { 
+    let statusCode = 500
+    let message = 'Internal server error!'
+    let errorCode = 'INTERNAL_SERVER_ERROR'
+    let isOperational = false
+
+    if (error instanceof AppError) {
+        statusCode = error.statusCode
+        message = error.message
+        errorCode = error.errorCode
+        isOperational = error.isOperational
+    }
+
+    // DEV MODE
+    if (config.nodeEnv === 'development' || !isOperational) {
+        // The logger is configured with format.errors({ stack: true }), so it will automatically handle the stack.
+        logger.error(`Error on ${req.method} ${req.originalUrl}`, {
+            error,
+            url: req.url,
+            method: req.method,
+            ip: req.ip,
+            userAgent: req.get('User-Agent')
+        });
+    }
+
+    // Hide internal details
+    if (config.nodeEnv === 'production' && !isOperational) {
+        message = 'Something went wrong. Please try again later.';
+    }
+
+    const response: IApiResponse = {
+        success: false,
+        message,
+        error: {
+            code: errorCode,
+            ...(config.nodeEnv === 'development' && { stack: error.stack }) // If it's true, the line becomes only { stack: error.stack }
+        }
+    }
+
+    res.status(statusCode).json(response)
+}
---
+++ src/middlewares/rate_limiter.ts
@@ -0,0 +1,27 @@
+import rateLimit from 'express-rate-limit';
+import { config } from '../config';
+import { Request } from 'express';
+
+export const globalRatelimit = rateLimit({
+    windowMs: config.rateLimit.windowMS,
+    max: config.rateLimit.maxRequests, 
+    message: {
+        success: false,
+        message: 'Too many requests, please try again later.'
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+})
+
+export const authRateLimit = rateLimit({
+    windowMs: config.rateLimit.windowMS, // 15 minutes
+    max: 5, // limit each IP to 5 requests per windowMs for auth endpoints
+    message: {
+        success: false,
+        message: 'Too many authentication attempts, please try again later.'
+    },
+    skip: (req: Request) => {
+        // Skip rate limiting for successful requests
+        return req.method === 'GET';
+    }
+});
