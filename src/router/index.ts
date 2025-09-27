1ec7babf4e1b9dd5406a26faae18817fa3520898
maxim-ld <ayinde779@gmail.com>
2025-09-27 08:41:40 +0100

    refactor: change API versioning in routes to /api/v1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { authRouter } from './auth.router'
 
 const router = Router()
 
@@ -8,5 +9,7 @@ router.get('/health', (req, res) => {
         message: 'Health is wealth'
     })
 })
+
+router.use('/auth', authRouter)
 
 export default router
