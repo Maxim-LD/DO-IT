d84b61edd1f330583635d3d46e5ab7888d8054e0
maxim-ld <ayinde779@gmail.com>
2025-09-27 08:19:19 +0100

    feat: Enhanced AuthRepository to manage authentication records

--- src/repository/AuthRepository.ts
+++ src/repository/AuthRepository.ts
@@ -1,37 +1,35 @@
 import { Knex } from "knex";
 import { CreateAuthDTO, IAuth } from "../types/auth";
-import { IProvider } from "../types/provider";
 import { hashPassword } from "../utils/hash";
 import { BaseRepository } from "./BaseRepository";
 import { v4 as uuidv4 } from "uuid";
+import { BadRequestError } from "../utils/errors";
 
 export class AuthRepository extends BaseRepository<IAuth> {
     constructor() {
-        super('auth')
+        super('auth', 'sn')
     }
 
-    async createAuthRecord(input: CreateAuthDTO, trx?: Knex.Transaction): Promise<IAuth | null> {
-        const hashedSecret = await hashPassword(input.secret)
-
-        // add provider record
-        const provider = await this.db('providers').where('name', input.identifier).first();
-        if (!provider) throw new Error("Invalid provider");
-        
-        const authdata: Partial<IAuth> = {
-            id: uuidv4(),
-            ...input,
-            provider_id: provider.id,
-            secret: hashedSecret,
-            is_email_verified: false,
-            is_phone_verified: false,
-            created_at: new Date(),
-            updated_at: new Date()
-        }
-
-        return await this.create(authdata)
-    }  
+    async createAuthRecord(input: CreateAuthDTO): Promise<IAuth | null> {
+        return await this.db.transaction(async (trx) => {
+            // Get provider record
+            const provider = await this.db('providers').where('name', input.provider_name).first();
+            if (!provider) throw new BadRequestError("Unknown provider type");
     
-    async generateToken() {
-        
-    }
+            if (!input.secret) throw new BadRequestError("Missing secret")
+            const hashedSecret = await hashPassword(input.secret)
+            
+            const authdata: Partial<IAuth> = {
+                id: uuidv4(),
+                user_sn: input.user_sn,
+                provider_sn: provider.sn,
+                provider_identity: input.provider_identity,
+                hashed_secret: hashedSecret,
+                created_at: new Date(),
+                updated_at: new Date()
+            }
+    
+            return await this.create(authdata, trx)
+        })
+    }  
 }
