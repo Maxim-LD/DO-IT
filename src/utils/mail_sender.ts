781ad9a492dc0efb67e00087b25fef619a395d9e
maxim-ld <ayinde779@gmail.com>
2025-09-27 08:20:49 +0100

    feat:  Integrated nodemailer for sending emails with SMTP configuration.

---
+++ src/utils/mail_sender.ts
@@ -0,0 +1,31 @@
+import nodemailer from 'nodemailer'
+import { config } from '../config'
+import { logger } from './logger'
+
+export const sendEmail = async (to: string, subject: string, html: string): Promise<void> => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: config.smtp.email,
+                pass: config.smtp.password
+            }
+        })
+
+        const mailOptions = {
+            from: `'DO-IT' <${config.smtp.email}>`,
+            to,
+            subject,
+            html
+        }
+
+        const info = await transporter.sendMail(mailOptions)
+        logger.info("Email sent:", { response: info.response })
+
+    } catch (error) {
+        if (error instanceof Error) {
+            throw new Error(` Email send failed: ${error.message}`)
+        }
+        throw error
+    }
+}
