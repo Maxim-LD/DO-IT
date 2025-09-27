0af5b9e5706392d12dd01e5d0b82b01628be6608
maxim-ld <ayinde779@gmail.com>
2025-09-27 08:37:31 +0100

    refactor: change API versioning in routes to /api/v1

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -2,18 +2,18 @@ export interface IAuth {
     sn: bigint
     id: string,
     user_sn: bigint
-    provider_id: string
-    identifier: string
-    secret: string
-    is_email_verified: boolean
-    is_phone_verified: boolean
+    provider_sn: string
+    provider_name: string
+    provider_identity: string
+    hashed_secret: string
     created_at: Date;
     updated_at: Date;
 }
 
 export interface CreateAuthDTO {
     user_sn: bigint
-    provider_id?: string
-    identifier: string
-    secret: string
+    provider_sn?: string
+    provider_name?: string
+    provider_identity: string
+    secret?: string
 }
