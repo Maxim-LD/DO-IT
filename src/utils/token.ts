5db57800ddeabf89fea72e27de490bda23b68a81
maxim-ld <ayinde779@gmail.com>
2025-09-27 08:20:16 +0100

    feat: Added token generation and verification utilities for email confirmation.

---
+++ src/utils/token.ts
@@ -0,0 +1,27 @@
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+export const generateToken = (
+    payload: Record<string, any>,
+    secret: string,
+    expiresIn: jwt.SignOptions['expiresIn']
+): string => {
+    try {
+        const options: jwt.SignOptions = { expiresIn }
+
+        return jwt.sign(payload, secret, options)
+    } catch (error) {
+        if (error instanceof Error) {
+            throw new Error(` Token generation failed: ${error.message}`)
+        }
+        throw error
+    }
+}
+
+export const verifyToken = (token: string, secretKey: string): JwtPayload => {
+    const decoded = jwt.verify(token, secretKey)
+
+    if (typeof decoded === 'string') {
+        throw new Error('Invalid token payload!')
+    }
+    return decoded
+}
