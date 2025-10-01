eb0dcdf44774492d86ebb5bfbe1218071c01061d
maxim-ld <ayinde779@gmail.com>
2025-10-01 16:24:32 +0100

    fix: Enhanced AuthService with login functionality and token issuance.

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,22 +1,30 @@
 import db from "../config/db";
-import { secretConfig } from "../config";
+import { config, secretConfig } from "../config";
 import { AuthRepository } from "../repository/AuthRepository";
 import { UserRepository } from "../repository/UserRepository";
 import { CreateAuthDTO } from "../types/auth";
 import { CreateUserDTO, IUser } from "../types/user";
-import { ConflictError } from "../utils/errors";
+import { ConflictError, NotFoundError, UnauthorizedError } from "../utils/errors";
 import { generateToken, verifyToken } from "../utils/token";
+import { comparePassword } from "../utils/hash";
+import { ILoginResponse } from "../types/api_response";
+import { setCache } from "../utils/caching";
+import { TokenService } from "./TokenService";
 
 export class AuthService {
     private userRepository: UserRepository
     private authRepository: AuthRepository
+    private emailSecret = secretConfig.emailSecret
+    private secretKey = secretConfig.secretKey
+    private refreshKey = secretConfig.refreshSecret
 
     constructor() {
         this.userRepository = new UserRepository()
         this.authRepository = new AuthRepository()
     }
 
-    async register(userInput: CreateUserDTO): Promise<IUser | null> {
+
+    async registerUser(userInput: CreateUserDTO): Promise<IUser | null> {
         return await db.transaction(async (trx) => {  
             
             const userPayload: CreateUserDTO  = {
@@ -68,16 +76,34 @@ export class AuthService {
     }
 
     async verifyEmailToken(token: string): Promise<string | null> {
-        const emailSecret = secretConfig.emailSecret
-        const secretKey = secretConfig.secretKey
-
-        const payload = verifyToken(token, emailSecret)
+        const payload = verifyToken(token, this.emailSecret)
         const userId = payload.user_id
 
         await this.userRepository.update(userId, { is_email_verified: true })
 
-        const accessToken = generateToken({ user_id: userId }, secretKey, '2h')
+        const accessToken = generateToken({ user_id: userId }, this.secretKey, '2h')
         
         return accessToken
+    }
+
+    async loginUser(key: string, value: string, secret: string): Promise<ILoginResponse> {
+        return db.transaction(async (trx) => {
+            // 1. Check if user exist
+            const user = await this.userRepository.findOne(key, value, trx)
+            if (!user) throw new NotFoundError('User does not exist!')
+            
+            // 2. Check auth record for password match
+            const auth = await this.authRepository.findOne('user_sn', user.sn, trx)
+            if (!auth) throw new NotFoundError('No auth record for user!')
+
+            // 3. Compare password
+            const isMatch = await comparePassword(secret, auth.hashed_secret)
+            if (!isMatch) throw new UnauthorizedError('Invalid credentials provided!')
+            
+            // 4. Generate auth tokens
+            const { accessToken, refreshToken } = await TokenService.issueAuthTokens(user)
+            
+            return { user, accessToken, refreshToken }
+        })
     }
 }
